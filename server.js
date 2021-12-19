c5d5e82132919efef518cb6978b20d02ad9c1759
hpurring <hilarypurrington@gmail.com>
2021-12-19 17:02:34 -0500

    delete notes

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const notes = require('./Develop/db/db.json');
-
+const { v4: uuidv4 } = require('uuid');
+console.log(notes);
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -19,28 +20,30 @@ app.get('/', (req, res) => {
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
 });
-// GET * html route
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
-});
 
 // GET /api/notes API route
 app.get('/api/notes', (req, res) => {
     res.json(notes);
 });
 
+// GET * html route
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
+});
+
+
 // POST /api/notes
 app.post('/api/notes', (req, res) => {
-    req.body.id = notes.length;
-    // come up with random generator for above, otherwise deleting will mess this up
+    req.body.id = uuidv4();
     const note = createNewNote(req.body, notes);
     res.json(note);
-    // if (!validateNote(req.body)) {
-    //     res.status(400).send('The note is not properly formatted');
-    // } else {
-        
-    // }
 });
+
+app.delete('/api/notes/:id', (req, res) => {
+    deleteNote(req.body.id, notes);
+    res.json(req.body);
+});
+
 
 app.listen(3001, () => {
     console.log(`API server now on port http://localhost:${PORT}`);
@@ -51,8 +54,19 @@ function createNewNote(body, notes) {
     notes.push(note);
     fs.writeFileSync(
         path.join(__dirname, './Develop/db/db.json'),
-        JSON.stringify({ notes: notesArray }, null, 2)
+        JSON.stringify(notes)
     );
-
     return note;
+};
+
+function deleteNote(id, note) {
+    for (i in notes) {
+        if(notes[i].id === req.body.id) {
+            notes.splice(x, 1);
+        }
+    };
+    fs.writeFileSync(
+        path.join(__dirname, './Develop/db/db.json'),
+        JSON.stringify(notes)
+    );
 };
